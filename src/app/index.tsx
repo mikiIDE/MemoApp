e03a961d2e9c02045ca104f588a0aa04c4b8ef41
mikiIDE <kinoko.miki@gmail.com>
2025-02-06 15:29:10 +0900

    set vector-icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,8 @@
-import { View, StyleSheet } from 'react-native'
-import Header from "../components/Header " // 自分で作成したものだから？{}なし！
-import MemoListItem from "../components/MemoListItem"
-import CircleButton from "../components/CircleButton"
+import { Redirect } from "expo-router"
 
-const Index = ():JSX.Element => {
-    return (
-        // まずは全体を囲むView
-    <View style={styles.container}>
-
-{/* コンポーネント */}
-    <Header />
-        <View>
-{/* コンポーネント */}
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        </View>
-{/* コンポーネント */}
-        <CircleButton>+</CircleButton>
-    </View>
-    )
+const Index = () => {
+    return <Redirect href="memo/detail" />
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1, //この要素を画面いっぱいに広げる、という指示
-        backgroundColor:'#FFFFFF'
-    }
-})
 
 export default Index
---
+++ src/app/memo/list.tsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet } from 'react-native'
+import Header from "../../components/Header " // 自分で作成したものだから？{}なし！
+import MemoListItem from "../../components/MemoListItem"
+import CircleButton from "../../components/CircleButton"
+import { Feather } from "@expo/vector-icons"
+
+const List = ():JSX.Element => {
+    return (
+        // まずは全体を囲むView
+    <View style={styles.container}>
+
+{/* コンポーネント */}
+    <Header />
+        <View>
+{/* コンポーネント */}
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
+        </View>
+{/* コンポーネント */}
+        <CircleButton>
+            <Feather name ="plus" size={40} />
+        </CircleButton>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1, //この要素を画面いっぱいに広げる、という指示
+        backgroundColor:'#FFFFFF'
+    }
+})
+
+export default List
