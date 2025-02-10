0112802f883ec6dfdacf13e84b69aacef4dd2afb
mikiIDE <kinoko.miki@gmail.com>
2025-02-10 15:34:09 +0900

    add icon using icomoon

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -2,7 +2,8 @@ import { View, StyleSheet } from 'react-native'
 import Header from "../../components/Header " // 自分で作成したものだから？{}なし！
 import MemoListItem from "../../components/MemoListItem"
 import CircleButton from "../../components/CircleButton"
-import { Feather } from "@expo/vector-icons"
+// import { Feather } from "@expo/vector-icons"
+import Icon from "../../components/Icon"
 
 const List = ():JSX.Element => {
     return (
@@ -19,7 +20,8 @@ const List = ():JSX.Element => {
         </View>
 {/* コンポーネント */}
         <CircleButton>
-            <Feather name ="plus" size={40} />
+            {/* <Feather name ="plus" size={40} /> */}
+            <Icon />
         </CircleButton>
     </View>
     )
