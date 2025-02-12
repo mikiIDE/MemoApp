a6ddd94fbf53f2c7784a581f2bc0c809812bacb9
mikiIDE <kinoko.miki@gmail.com>
2025-02-12 15:27:14 +0900

    set useEffect to check whether already login or not

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,21 @@
-import { Redirect } from "expo-router"
+import { Redirect,router } from "expo-router"
+import { useEffect  } from "react"
 
-const Index = () => {
-    return <Redirect href="auth/signup" />
+// ⬇️ログイン状態を監視するため
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../config"
+
+const Index = ():JSX.Element => {
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user !== null ){
+                router.replace("/memo/list")
+            }
+        })
+    })
+    return <Redirect href="auth/login" />
 }
+
 
 
 export default Index
